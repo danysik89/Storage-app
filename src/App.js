e6483f4d654d6f02bc51aeaa4e73d12d71873827
Lenovo-PC\Danylo <danilogoryachko@gmail.com>
2019-03-13 13:57:28 +0200

    install material and add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import HeaderAppBar from './components/header-app-bar'
+
 class App extends Component {
   render() {
     return (
-      <div>Hello App</div>
+      <div>
+        <HeaderAppBar />
+        <div>Hello App</div>
+      </div>
     );
   }
 }
