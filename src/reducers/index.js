ba8d819e1d8f201408a2e16926ca45e3c1393368
dhoriachko <dhoriachko@n-ix.com>
2019-04-04 18:52:41 +0300

    start implementing redux

---
+++ src/reducers/index.js
@@ -0,0 +1,26 @@
+const initialState = {
+  storageData: [
+    {title: 'Pizza', number: 23, price: 13, id: '01'},
+    {title: 'Burger', number: 42, price: 7, id: '02'},
+    {title: 'Hot dog', number: 26, price: 6, id: '03'},
+    {title: 'Steak', number: 8, price: 89, id: '04'},
+    {title: 'Cruasan', number: 13, price: 12, id: '05'}
+  ], 
+  items: []
+};
+
+const reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case 'ITEMS_LOADED':
+      return {
+        items: action.payload
+      };
+      
+    default:
+     return state;
+  }
+
+};
+
+export default reducer;
