280fc0fc38730ba14b22a75926101d35ab505d70
Lenovo-PC\Danylo <danilogoryachko@gmail.com>
2019-04-08 01:59:56 +0300

    PR correction

--- src/services/itemstore-service.js
+++ src/services/itemstore-service.js
@@ -2,8 +2,11 @@ export default class ItemstoreService {
     
     getItems() {
         return [
-            {title: 'Poroshenko', number: 23, price: 13, id: '01'},
-            {title: 'Zelensky', number: 42, price: 7, id: '02'}
+            {title: 'Pizza', number: 23, price: 13, id: '01'},
+            {title: 'Burger', number: 42, price: 7, id: '02'},
+            {title: 'Hot dog', number: 26, price: 6, id: '03'},
+            {title: 'Steak', number: 8, price: 89, id: '04'},
+            {title: 'Cruasan', number: 13, price: 12, id: '05'}
         ];
     }
 
