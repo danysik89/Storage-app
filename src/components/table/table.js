f685159a13e2350269187acc8938c83f4d1eb611
dhoriachko <dhoriachko@n-ix.com>
2019-04-07 21:24:57 +0300

    read data from store

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import TableItem from '../table-item'
 import './table.css';
 
-const Table = ({tableData}) => {
+import { connect } from 'react-redux'
+
+// const Table = ({tableData}) => {
+class Table extends Component {
       
-  const items = tableData.map((item) => {
-    return <TableItem key={item.id} itemData={item}/>
-  });
+  render() {
+    const { items } = this.props;
+    const tableData = items.map((item) => {
+      return <TableItem key={item.id} itemData={item}/>
+    });
+
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Type</th>
+            <th scope="col">Number</th>
+            <th scope="col">Price</th>
+            {/* Action component? */}
+            <th scope="col" className="action-col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData}
+        </tbody>
+      </table>
+    );
+  }
   
-  return (
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">Type</th>
-          <th scope="col">Number</th>
-          <th scope="col">Price</th>
-          {/* Action component? */}
-          <th scope="col" className="action-col">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items}
-      </tbody>
-    </table>
-  );
 };
 
-export default Table;
+const mapStateToProps = ({ items }) => {
+  return { items }
+}
+
+export default connect(mapStateToProps)(Table);
